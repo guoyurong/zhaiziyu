f4f0027cd2a038aff3f8faea7f9c357df1081f1a
翟 <zzy@10.1.62.8>
2019-06-03 09:12:19 +0800

    项目

--- bawei-cms-v1.0/src/main/java/com/bawei/controller/admin/ArcTypeAdminControlle.java
+++ bawei-cms-v1.0/src/main/java/com/bawei/controller/admin/ArcTypeAdminControlle.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.ContextLoader;
 
 import com.bawei.entity.ArcType;
+import com.bawei.entity.Article;
 import com.bawei.entity.Link;
 import com.bawei.entity.PageBean;
 import com.bawei.service.ArcTypeService;
@@ -40,51 +41,17 @@ public class ArcTypeAdminControlle {
 	private ArticleService articleService;
 	
 	@RequestMapping("/list")
-	public String list(@RequestParam(value="page",required=false)String page,
-			@RequestParam(value="rows",required=false)String rows,
+	public String list(@RequestParam(value="page")String page,
+			@RequestParam(value="rows")String rows,
 			HttpServletResponse response) throws Exception{
 		PageBean pageBean = new PageBean(Integer.parseInt(page), Integer.parseInt(rows));
 		HashMap<String, Object> map = new HashMap<String, Object>();
-		map.put("start", pageBean.getStart());
-		map.put("size", pageBean.getPageSize());
-		List<ArcType> list = arcTypeService.list(map);
+		List<Article> list = articleService.list(map);
 		JSONArray jsonArray = JSONArray.fromObject(list);
-		Long total = arcTypeService.getTotal(map);
+		Long total = articleService.getTotal(map);
 		JSONObject result = new JSONObject();
 		result.put("rows", jsonArray);
 		result.put("total", total);
-		ResponseUtil.write(response, result);
-		return null;
-	}
-	
-	@RequestMapping("/save")
-	public String save(ArcType arcType,HttpServletRequest request,HttpServletResponse response) throws Exception{
-		int resultes=0;
-		if (arcType.getId() != null) {
-			resultes=arcTypeService.add(arcType);
-		}else{
-			resultes=arcTypeService.update(arcType);
-		}
-		JSONObject result = new JSONObject();
-		if (resultes>0) {
-			initComponet.refreshSystem(ContextLoader.getCurrentWebApplicationContext().getServletContext());
-			result.put("success", true);
-		}else{
-			result.put("success", false);
-		}
-		ResponseUtil.write(response, result);
-		return null;
-	}
-	
-	@RequestMapping("/delete")
-	public String delete(@RequestParam(value="ids")String ids,HttpServletResponse response) throws Exception{
-		String[] moreid = ids.split(",");
-		for (String id : moreid) {
-			arcTypeService.delete(Integer.parseInt(id));
-		}
-		initComponet.refreshSystem(ContextLoader.getCurrentWebApplicationContext().getServletContext());
-		JSONObject result = new JSONObject();
-		result.put("success", true);
 		ResponseUtil.write(response, result);
 		return null;
 	}
